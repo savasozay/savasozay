3e572616cd02e347d86b5c6006d54ad5bf001825
Savas Ozay <mail@savasozay.com>
2021-01-30 20:10:22 +0100

    Structure

--- site/snippets/default.php
+++ site/snippets/default.php
@@ -1,6 +1,10 @@
 <section class="content unit w-7-8">
 	<article>
-	<?php echo $page->text()->kirbytext() ?>
+		<h1><?= $page->title()->html() ?></h1>
+		<h2><?php echo $page->details()->html() ?></h2>
+
+
+		<?php echo $page->text()->kirbytext() ?>
 	</article>
 </section>
 
